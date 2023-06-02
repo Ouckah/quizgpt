a2f2ac88c6a4da1f033ff0f673a86671d3419f11
Aidan <datbluemushroom@gmail.com>
2023-06-02 03:10:48 -0400

    ✨ Styling + Improved Generation

--- app/generate/components/Generator/index.tsx
+++ app/generate/components/Generator/index.tsx
@@ -1,20 +1,15 @@
 "use client"
-import { useState, ChangeEvent, useRef } from "react";
+import { useState, useRef } from "react";
 
-import { ConversationChain } from "langchain/chains";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  SystemMessagePromptTemplate,
-  MessagesPlaceholder,
-} from "langchain/prompts";
-import { BufferMemory } from "langchain/memory";
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
 
 const Generator = ({  }) => {
 
     const KEY = ""
     
+    const topic = "Biology";
+
     // creates a chat to respond to the user's request
     const chat = new ChatOpenAI({
         openAIApiKey: KEY,
@@ -23,78 +18,106 @@ const Generator = ({  }) => {
         maxConcurrency: 5,
     });
 
-    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-        SystemMessagePromptTemplate.fromTemplate(
-            "The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know."
-        ),
-        new MessagesPlaceholder("trivia"),
-        HumanMessagePromptTemplate.fromTemplate("{input}"),
-    ]);
-
     const generateHandler = async () => {
 
-        // // clear current text
-        // setQuestion("");
+        // clear current question
+        setQuestion("");
+        setFinished(false);
+        setAnswer("");
 
-        chain.current = new ConversationChain({
-            memory: new BufferMemory({ returnMessages: true, memoryKey: "trivia" }),
-            prompt: chatPrompt,
-            llm: chat,
-        });
-        
         // makes an API call to OpenAI with the following context
-        await chain.current.call({ input: "Give me a question about video games." },
-            [
-                {
-                    // as tokens are fed, append to end of response and display
-                    handleLLMNewToken(token: string) {
-                        setQuestion((prevText: string) => prevText + token);
-                    },
+        await chat.call([
+                    
+            // context that the AI is a job recruiter fixing resumes
+            new SystemChatMessage(
+                `You are a trivia / quizbowl question generator that provides questions and answers based on certain topics.
+                You provide only unique questions that you have not asked before.
+                You will only ask the question with no other text surrounding the question.
+                Do not include questions whos answers are any of the following: ${answers}`
+            ),
+            
+            // context of a human asking to have their resume rewritten
+            new HumanChatMessage(
+                `Give me a question on ${topic} and only state the question. Do not include any questions that have the answer of any of these: ${answers}`
+            ),
+        ],
+        undefined,
+        [
+            {
+                // as tokens are fed, append to end of response and display
+                handleLLMNewToken(token) {
+                    setQuestion((prevText) => prevText + token);
                 },
-            ]
-        );
+            },
+        ]);
+
+        // flag question as finished
+        setFinished(true);
+
+        setQuestions((prevQuestions) => [...prevQuestions, question]);
+
     }
 
-    const submitHandler = async () => {
+    const answerHandler = async () => {
+
+        if (!flipped) {
+            generateHandler();
+            setFlipped(!flipped);
+            return;
+        }
+
+        setFlipped(!flipped)
+
+        // clear current question and answer
+        setQuestion("");
+        setAnswer("");
+
+        let localAnswer = "";
 
         // makes an API call to OpenAI with the following context
-        await chain.current.call({ input: `I believe the answer is ${answer}` },
-            [
-                {
-                    // as tokens are fed, append to end of response and display
-                    handleLLMNewToken(token: string) {
-                        setResponse((prevText: string) => prevText + token);
-                    },
+        await chat.call([
+            
+            // context of a human asking to have their resume rewritten
+            new HumanChatMessage(
+                `What is the answer to ${question}. Only generate the answer and no other text.`
+            ),
+        ],
+        undefined,
+        [
+            {
+                // as tokens are fed, append to end of response and display
+                handleLLMNewToken(token) {
+                    setAnswer((prevText) => prevText + token);
+                    localAnswer += token;
                 },
-            ]
-        );
+            },
+        ])
+
+        // add answer to answer list
+        setAnswers((prevAnswers) => [...prevAnswers, localAnswer]);
+
+        console.log(answers)
+
     }
     
     const [question, setQuestion] = useState("");
+    const [finished, setFinished] = useState(false);
     const [answer, setAnswer] = useState("");
-    const [response, setResponse] = useState("");
-    const chain = useRef(new ConversationChain({
-        memory: new BufferMemory({ returnMessages: true, memoryKey: "trivia" }),
-        prompt: chatPrompt,
-        llm: chat,
-    }));
+
+    const [questions, setQuestions] = useState<string[]>([]);
+    const [answers, setAnswers] = useState<string[]>([]);
+
+    const [flipped, setFlipped] = useState(false);
 
     return (
         <>
         
-            <div className="flex w-full h-full flex-col rounded-xl bg-gray-900 shadow-blue border-white border-2">
-                <p>{question.length > 0 && question}</p>
-                <button onClick={generateHandler}>Generate</button>
-
-                {question.length > 0 ? (
-                    <>
-                        <input onChange={(e) => setAnswer(e.target.value)} className="text-black"></input>
-                        <button onClick={submitHandler}>Submit</button>
-                        <p>{answer.length > 0 && answer}</p>
-                        <p>{response.length > 0 && response}</p>
-                    </>
-                ) : (<></>)}
-            </div>
+            <button 
+                className={`flex w-full h-full flex-col justify-center items-center rounded-xl bg-gray-900 shadow-blue border-white border-2`}
+                onClick={answerHandler}
+            >
+                <p className="text-white font-bold text-2xl p-5">{question.length > 0 ? (question) : (answer)}</p>
+            </button>
         
         </>
     )
--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -3,13 +3,13 @@ import Generator from "./components/Generator";
 export default function Generate() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+                
             <h1>Generator</h1>
             <div className="flex h-[35em] w-full flex-row gap-8">
                 <div className="w-1/2 h-full">
                     <Generator />
                 </div>
-                <div className="flex w-1/2 h-full flex-col bg-blue-400">
+                <div className="flex w-1/2 h-full flex-col">
                     
                 </div>
             </div>
